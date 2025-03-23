ab49fdae0ccb7f152793ced2d4058bd353326059
BsgMax00 <bram.engels22@gmail.com>
2025-03-23 19:26:04 +0100

    added toFixed to class so that all prices have 2 decimal points

--- src/Classes/Invoice.js
+++ src/Classes/Invoice.js
@@ -2,7 +2,7 @@ class Invoice {
     constructor(Checked, Name, Price, Date, Status, Download){
         this.Checked = Checked;
         this.Name = Name;
-        this.Price = Price;
+        this.Price = Price.toFixed(2);
         this.Date = Date;
         this.Status = Status;
         this.Download = Download;
